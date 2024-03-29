8128ae376b97d839386155e89c0198e8329cbf2a
Rolek <Karolsusek2007@gmail.com>
2024-03-29 16:27:02 +0100

    [modified]  projects.html script.js [added] spirit.css spirit.html

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ var typed = new Typed('#typed', {
 	loop: true,
 	cursorChar: '_',
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+	var link = document.getElementById('link');
+
+	link.addEventListener('click', function (event) {
+		event.preventDefault(); // Zapobiegamy domyślnej akcji przekierowania po kliknięciu na link
+		window.location.href = link.href; // Przenosimy użytkownika do docelowej strony
+	});
+});
